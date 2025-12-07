f1e1bd51e4a20c2c5a4cea7f4502bd8f4fcde2a1
Prefna Vagheni <prefnavagheni54@gmail.com>
2025-12-07 21:03:24 +0300

    Edit: Alias

--- app/admin/bookings/[id]/page.js
+++ app/admin/bookings/[id]/page.js
@@ -1,4 +1,4 @@
-import { supabaseAdmin } from '@/lib/supabase';
+import { supabaseAdmin } from '../../lib/supabase';
 import { notFound } from 'next/navigation';
 import Link from 'next/link';
 import CancelBookingButton from '@/_components/CancelBookingButton';
