923dc5ff9b83aac360852dcfddbfc66bd273f448
Prefna Vagheni <prefnavagheni54@gmail.com>
2025-12-21 23:24:24 +0300

    Polish component

--- app/admin/staff/page.js
+++ app/admin/staff/page.js
@@ -133,7 +133,6 @@ function StaffCard({ staff }) {
         )}
 
         <div className="space-y-2">
-          {/* Edit Details Button (Colors already contrast well) */}
           <Link
             href={`/admin/staff/${staff.id}`}
             className="block text-center px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 font-medium transition duration-150"
