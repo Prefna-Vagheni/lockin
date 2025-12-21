406b054f91dd962387aff4186fb8f29b331978ad
Prefna Vagheni <prefnavagheni54@gmail.com>
2025-12-21 23:24:14 +0300

    Polish component

--- app/admin/staff/page.js
+++ app/admin/staff/page.js
@@ -140,7 +140,6 @@ function StaffCard({ staff }) {
           >
             Edit Details
           </Link>
-          {/* Set Working Hours Button */}
           <Link
             href={`/admin/staff/${staff.id}/availability`}
             className="block text-center px-4 py-2 bg-gray-100 dark:bg-gray-700 text-gray-700 dark:text-gray-200 rounded hover:bg-gray-200 dark:hover:bg-gray-600 font-medium transition duration-150"
