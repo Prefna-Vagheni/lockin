ef999ebdaa75d83fce2ed74c0304edff16040faf
Prefna Vagheni <prefnavagheni54@gmail.com>
2026-01-02 21:05:02 +0300

    Polish components

--- app/_components/EditServiceForm.js
+++ app/_components/EditServiceForm.js
@@ -99,7 +99,6 @@ export default function EditServiceForm({ service }) {
           />
         </div>
 
-        {/* Duration */}
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-2">
             Duration (minutes) *
