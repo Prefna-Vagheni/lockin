d00f52f5dc7f19124fc1ed7dff8cd95aa3096fdc
Prefna Vagheni <prefnavagheni54@gmail.com>
2026-01-04 21:51:16 +0300

    Polish components

--- app/_components/DashboardHeader.js
+++ app/_components/DashboardHeader.js
@@ -35,7 +35,7 @@ export default function DashboardHeader({ user }) {
             </Link>
           </div>
 
-          {/* :   ( on ) */}
+          {/* :    */}
           <div className="hidden md:flex items-center space-x-4">
             <ThemeToggle />
             <div className="flex items-center space-x-3">
