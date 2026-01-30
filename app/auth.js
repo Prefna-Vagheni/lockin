193649d15848f8a4d800e432a0a3f121e6af97ef
Prefna Vagheni <prefnavagheni54@gmail.com>
2026-01-31 01:28:48 +0300

    Fix Google OAuth for production

--- app/auth.js
+++ app/auth.js
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
 );
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
@@ -84,4 +84,5 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   pages: {
     signIn: '/auth/signin',
   },
+  trustHost: true,
 });
